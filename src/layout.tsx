7057d87492b93f4f02ae100e029da6eac6c3e8b3
Murat Kaan Ilhan <kaan.ilhan@proton.me>
2024-09-30 14:26:18 +0300

    first commit

---
+++ src/layout.tsx
@@ -0,0 +1,17 @@
+import React from 'react';
+
+ const  RootLayout : React.FC<any> = ({ children})=> {
+  return (
+   <div>  
+
+    <div>kaan</div>
+
+    <div>
+      {children}
+
+    </div>
+    </div>
+  );
+}
+
+export default RootLayout
---
+++ src/page.tsx
@@ -0,0 +1,17 @@
+import RootLayout from './layout';
+import React from 'react';
+const Home : React.FC<any>= ()=> {
+  return (
+
+    <RootLayout>
+
+            <div>from page</div>
+
+    </RootLayout>
+
+
+  );
+}
+
+
+export default Home
